9f516b7e4164d7a543d3581701df2e6e25718e65
fivetrue <kwonojin@KwonOjinui-MacBook-Pro.local>
2017-01-18 10:31:18 +0900

    init Git.

---
+++ app/src/main/java/com/fivetrue/market/memo/MemoApplication.java
@@ -0,0 +1,18 @@
+package com.fivetrue.market.memo;
+
+import android.app.Application;
+
+/**
+ * Created by kwonojin on 2017. 1. 18..
+ */
+
+public class MemoApplication extends Application {
+
+    private static final String TAG = "MemoApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    
+}
